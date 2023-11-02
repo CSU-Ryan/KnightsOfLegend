ae65f9aa7c99fbea35146d4a9d276723be646627
CSU-Ryan <enderbro@colostate.edu>
2023-11-02 15:18:13 -0600

    Implements Knight methods and tests

--- src/Knight.java
+++ src/Knight.java
@@ -4,71 +4,103 @@ public class Knight extends MOB {
     protected int xp;
 
 
-    public Knight(int id, String name, int hp, int armor, int hitModifier, DiceType damageDie, int xp) {
-        //TODO: implement constructor
-        super(name, hp, armor, hitModifier, damageDie);
+    public Knight(int id, String name, int maxHP, int armor, int hitModifier, DiceType damageDie, int xp) {
+        super(name, maxHP, armor, hitModifier, damageDie);
+        this.activeFortune = null;
+        this.id = id;
+        this.xp = xp;
     }
 
     @Override
     public String toString() {
-        //TODO: implement
-        return "";
+        return "+============================+\n" +
+                String.format("| %-27s|%n", getName()) +
+                String.format("| id: %-23d|%n", getId()) +
+                "|                            |\n" +
+                String.format("| Health: %-6d     XP: %-4d|%n", getHP(), getXP())  +
+                String.format("|  Power: %-6s  Armor: %-4d|%n", getDamageDie(), getArmor()) +
+                "|                            |\n" +
+                "+============================+";
     }
 
     public String toCSV() {
-        //TODO: implement
-        return "";
+        return String.format("%s,%d,%d,%d,%s,%d",
+                getName(), getMaxHP(), getArmor(), getHitModifier(), getDamageDie(), getXP());
     }
 
     public Integer getId() {
-        //TODO: implement
-        return null;
+        return id;
     }
 
     public int getXP() {
-        //TODO: implement
-        return -1;
+        return xp;
     }
 
     public void addXP(int xp) {
-        //TODO: implement
+        this.xp += xp;
     }
 
     @Override
     public int getMaxHP() {
-        //TODO: implement
-        return -1;
+        return super.getMaxHP();
     }
 
     @Override
     public int getArmor() {
-        //TODO: implement
-        return -1;
+        return super.getArmor();
     }
 
     @Override
     public DiceType getDamageDie() {
-        //TODO: implement
-        return null;
+        return super.getDamageDie();
     }
 
     @Override
     public int getHitModifier() {
-        //TODO: implement
-        return -1;
+        return super.getHitModifier();
     }
 
     public void setActiveFortune(Fortune fortune) {
-        //TODO: implement
+        activeFortune = fortune;
     }
 
     public Fortune getActiveFortune() {
-        //TODO: implement
-        return null;
+        return activeFortune;
     }
 
     public static void main(String[] args) {
-        
+
+        Knight knight1 = new Knight(1, "knight1", 10, 1, -1, DiceType.D20, 0);
+        //TODO: implement fortune use
+        // knight1.setActiveFortune(Fortune());
+
+        System.out.println("knight1 CSV: " + knight1.toCSV());
+        System.out.println();
+
+        System.out.println(knight1);
+        System.out.println("hitMod: " + knight1.getHitModifier());
+        System.out.println("MaxHP: " + knight1.getMaxHP());
+        System.out.println("Damage: " + knight1.getDamage());
+        System.out.println();
+
+        System.out.println("Damaging for 5HP:");
+        knight1.addDamage(5);
+        System.out.println(knight1);
+        System.out.println();
+
+        System.out.println("Damaging for 7HP:");
+        knight1.addDamage(7);
+        System.out.println(knight1);
+        System.out.println();
+
+        System.out.println("Resetting knight1 damage");
+        knight1.resetDamage();
+        System.out.println(knight1);
+        System.out.println();
+
+        System.out.println("Adding 37 XP:");
+        knight1.addXP(37);
+        System.out.println(knight1);
     }
     
 }
