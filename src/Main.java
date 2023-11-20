e07dce7bc6f908aab06131d1f1ae27c29c9dbd65
CSU-Ryan <enderbro@colostate.edu>
2023-11-19 23:46:15 -0700

    Creates and implements Main.
    To be seen if functioning.

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+import IO.*;
+import GameEngine.CombatEngine;
+
+public class Main {
+    private static String gamedata = "GameData/gamedata.csv";
+    private static String savedata = "GameData/knights.csv";
+
+    public Main() {}
+
+    public static void main(String[] args) {
+        processArgs(args);
+
+        GameData data = new CSVGameData(gamedata, savedata);
+        GameView view = new ConsoleView();
+        CombatEngine engine = new CombatEngine(data, view);
+        GameController controller = new GameController(data, view, engine);
+
+        controller.start();
+    }
+
+    private static void processArgs(String[] args) {
+        for (String arg : args) {
+            Scanner scanner = new Scanner(arg);
+            scanner.useDelimiter("=");
+            String next;
+
+            while (scanner.hasNext()) {
+                next = scanner.next();
+                if (next.equals("--data")) {
+                    gamedata = scanner.next();
+                    next = scanner.next();
+                }
+                savedata = next;
+            }
+        }
+    }
+}
