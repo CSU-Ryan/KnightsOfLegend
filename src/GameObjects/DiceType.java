8690f915ee00cbf09f51ef74af81c3bdcf0cde1a
CSU-Ryan <enderbro@colostate.edu>
2023-11-26 17:56:17 +0100

    Documents DiceType.

--- src/GameObjects/DiceType.java
+++ src/GameObjects/DiceType.java
@@ -1,13 +1,27 @@
 package GameObjects;
 
+/**
+ * Enumerates the possible types of dice (i.e. faces on die).
+ */
 public enum DiceType {
     D4, D6, D8, D10, D12, D20, NONE;
 
+    /**
+     * The name of the dice type. If NONE, returns `-`.
+     *
+     * @return a string of the dice type.
+     */
     @Override
     public String toString() {
         return (this != NONE) ? this.name() : "-";
     }
 
+    /**
+     * Turns a string into a DiceType. `-` becomes NONE.
+     *
+     * @param name string of DiceType
+     * @return the corresponding DiceType
+     */
     public static DiceType typeOf(String name) {
         return (name.equals("-")) ? DiceType.NONE : valueOf(name);
     }
