5d01bce5a226b4518e328c7b322f716f7f21fb64
CSU-Ryan <enderbro@colostate.edu>
2023-11-26 18:44:46 +0100

    Adds requirements of Attributes as a list to javadoc.

--- src/GameObjects/Attributes.java
+++ src/GameObjects/Attributes.java
@@ -1,12 +1,22 @@
 package GameObjects;
 
 /**
- * Attributes collection for typical game objects (monsters, knights, spells, fortunes, etc.).
+ * Attributes collection for typical game objects.<br>
+ * (monsters, knights, spells, fortunes, etc.).<br>
+ * <br>
+ * Ensures a game object has:
+ * <ul>
+ *     <li>An info card describing the object's stats<br> {@link #toString()}</li>
+ *     <li>An armor stat<br> {@link #getArmor()}</li>
+ *     <li>A hit-point (HP) level<br> {@link #getMaxHP()}</li>
+ *     <li>A {@link DiceType} for damage rolls<br> {@link #getDamageDie()}</li>
+ *     <li>A modifier for hit rolls<br> {@link #getHitModifier()}</li>
+ * </ul>
  */
 public interface Attributes {
 
     /**
-     * Creates a card of this object's stats.
+     * Creates an info card of this object's stats.
      *
      * @return a string of the info card.
      */
@@ -17,6 +27,7 @@ public interface Attributes {
      * Gets the armor value.
      *
      * @return the armor value.
+     * @see MOB#calculateHit(DiceSet, int)
      */
     int getArmor();
 
